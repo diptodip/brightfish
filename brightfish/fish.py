c6e7b80f883f1a62fa35e7c07fdb77db9ac9ed27
Diptodip Deb <d2d.tar.gz@gmail.com>
2019-03-20 15:51:00 -0400

    Clean/fix fish stepping and include environment stepping

--- brightfish/fish.py
+++ brightfish/fish.py
@@ -28,13 +28,20 @@ class BinocularFish(Fish):
         self.set_point -= self.learning_rate * update
 
         # update turn probabilities to turn towards area closer to set point
+        # first calculate differences from set point on both sides
         diff_left = np.abs(brightness_left - self.set_point)
         diff_right = np.abs(brightness_right - self.set_point)
-        diff_right_left = diff_right - diff_left
-        self.p_right += self.learning_rate * diff_right_left
-        self.p_left -= self.learning_rate * diff_right_left
+        # then calculate difference of differences
+        diff_left_right = diff_left - diff_right
+        # update turn probabilities appropriately
+        self.p_right += self.learning_rate * diff_left_right
+        self.p_left -= self.learning_rate * diff_left_right
+        # clip updated values to maintain valid probabilities
         self.p_right = np.clip(self.p_right, 0.0, 1.0)
         self.p_left = np.clip(self.p_left, 0.0, 1.0)
+
+        # step environment
+        environment.step()
 
         # return updated parameters
         return [self.set_point, self.p_right, self.p_left]
