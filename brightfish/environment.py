80983870887affff8b9363c24c8cdb0e694edd36
Diptodip Deb <d2d.tar.gz@gmail.com>
2019-03-20 15:47:38 -0400

    Rename to SinusoidalCircle and add proper parameters

--- brightfish/environment.py
+++ brightfish/environment.py
@@ -20,17 +20,21 @@ class Environment:
     def right_eye(self, heading):
         raise NotImplementedError
 
-class SinusoidalLine(Environment):
-    def __init__(self, shape,
-                       start = -np.pi,
-                       stop = np.pi):
-        super(SinusoidalLine, self).__init__(shape)
-        self.start = start
-        self.stop = stop
-        self.stage = np.sin(np.linspace(self.start,
-                                        self.stop,
-                                        steps=self.shape))
-        self.phase = 0
+class SinusoidalCircle(Environment):
+    def __init__(self,
+                 shape,
+                 dt = 1e-2,
+                 static=True):
+        super(SinusoidalCircle, self).__init__(shape)
+        self.start = 0
+        self.stop = 2 * np.pi
+        self.phase = 3 * np.pi / 2
+        self.dt = dt
+        self.stage = 0.5 * (1 + np.sin(np.linspace(self.start,
+                                            self.stop,
+                                            num=self.shape)
+                                       + self.phase))
+        self.static = static
 
     def step(self):
         if not self.static:
