7907b18f32d74988e4e039ee7cf52cea2cc0997a
Diptodip Deb <d2d.tar.gz@gmail.com>
2019-03-20 23:13:58 -0400

    Add documentation for base Environment class

--- brightfish/environment.py
+++ brightfish/environment.py
@@ -1,23 +1,56 @@
 import numpy as np
 
 class Environment:
+    """
+    Base class for defining environments. A base environment takes at least a
+    shape. Subclasses of Environment should implement methods for changes in the
+    environment, visibility from a left eye, and visibility from a right eye.
+
+    Args:
+	shape (tuple or int): Integer or tuple of integers defining the shape of
+	the environment.
+
+    Attributes:
+	shape (tuple or int): Integer or tuple of integers defining the shape of
+	the environment.
+
+	stage (``None`` or ``np.ndarray``): Contains actual data about the
+	environment, initialized to ``None``. The ``__init__()`` method of
+	subclasses of ``Environment`` should define the stage as an ndarray.
+    """
     def __init__(self, shape):
         self.shape = shape
         self.stage = None
 
     def step(self):
+        """Defines the behavior of the environment in one time step."""
         raise NotImplementedError
 
     def midpoint(self):
+        """Gives the middle coordinate/value of ``self.shape``."""
         if isinstance(self.shape, tuple):
             return tuple(s//2 for s in self.shape)
         else:
             return self.shape//2
 
     def left_eye(self, heading):
+        """
+	Returns the information observed by the left eye.
+
+	Args:
+	    heading (float): Gives the heading in radians from which to gather
+	    left eye information.
+        """
         raise NotImplementedError
  
     def right_eye(self, heading):
+        """
+	Returns the information observed by the right eye.
+
+	Args:
+	    heading (float): Gives the heading in radians from which to gather
+	    right eye information.
+        """
         raise NotImplementedError
 
 class SinusoidalCircle(Environment):
