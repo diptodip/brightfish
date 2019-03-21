7addc1cc8cf6026cc8062981121a073184c0a921
Diptodip Deb <d2d.tar.gz@gmail.com>
2019-03-20 23:52:26 -0400

    Fix tabs to spaces

--- brightfish/environment.py
+++ brightfish/environment.py
@@ -7,16 +7,16 @@ class Environment:
     environment, visibility from a left eye, and visibility from a right eye.
 
     Args:
-	shape (tuple or int): Integer or tuple of integers defining the shape of
-	the environment.
+        shape (tuple or int): Integer or tuple of integers defining the shape of
+        the environment.
 
     Attributes:
-	shape (tuple or int): Integer or tuple of integers defining the shape of
-	the environment.
+        shape (tuple or int): Integer or tuple of integers defining the shape of
+        the environment.
 
-	stage (``None`` or ``np.ndarray``): Contains actual data about the
-	environment, initialized to ``None``. The ``__init__()`` method of
-	subclasses of ``Environment`` should define the stage as an ndarray.
+        stage (``None`` or ``np.ndarray``): Contains actual data about the
+        environment, initialized to ``None``. The ``__init__()`` method of
+        subclasses of ``Environment`` should define the stage as an ndarray.
     """
     def __init__(self, shape):
         self.shape = shape
@@ -35,21 +35,21 @@ class Environment:
 
     def left_eye(self, heading):
         """
-	Returns the information observed by the left eye.
+        Returns the information observed by the left eye.
 
-	Args:
-	    heading (float): Gives the heading in radians from which to gather
-	    left eye information.
+        Args:
+            heading (float): Gives the heading in radians from which to gather
+            left eye information.
         """
         raise NotImplementedError
  
     def right_eye(self, heading):
         """
-	Returns the information observed by the right eye.
+        Returns the information observed by the right eye.
 
-	Args:
-	    heading (float): Gives the heading in radians from which to gather
-	    right eye information.
+        Args:
+            heading (float): Gives the heading in radians from which to gather
+            right eye information.
         """
         raise NotImplementedError
 
@@ -59,34 +59,34 @@ class SinusoidalCircle(Environment):
     at a heading of $\pi$ radians and is squished to the range [0, 1].
 
     Args:
-	shape (int): Integer defining the number of values of sine included in
-	``self.stage``.
+        shape (int): Integer defining the number of values of sine included in
+        ``self.stage``.
 
-	dt (float, optional): Float that designates how much the peak moves
-	counterclockwise in one time step if ``self.static`` is ``False``.
+        dt (float, optional): Float that designates how much the peak moves
+        counterclockwise in one time step if ``self.static`` is ``False``.
 
-	static (bool, optional): Boolean variable that determines whether peak
-	is stationary at each time step or moves autonomously at each time step.
+        static (bool, optional): Boolean variable that determines whether peak
+        is stationary at each time step or moves autonomously at each time step.
 
     Attributes:
-	shape (int): Integer defining the number of values of sine included in
-	``self.stage``.
+        shape (int): Integer defining the number of values of sine included in
+        ``self.stage``.
 
-	start (float): Contains the start heading for a circle, $0$.
+        start (float): Contains the start heading for a circle, $0$.
 
-	stop (float): Contains the end heading for a circle, $2\pi$.
+        stop (float): Contains the end heading for a circle, $2\pi$.
 
-	phase (float): Defines the phase of the sine function in radians.
-	Initialized to $\frac{3\pi}{2}$.
+        phase (float): Defines the phase of the sine function in radians.
+        Initialized to $\frac{3\pi}{2}$.
 
-	dt (float, optional): Float that designates how much the peak moves
-	counterclockwise in one time step if ``self.static`` is ``False``.
+        dt (float, optional): Float that designates how much the peak moves
+        counterclockwise in one time step if ``self.static`` is ``False``.
 
-	stage (None or ndarray): Flat ``np.ndarray`` that contains
-	``self.shape`` values of sine from $0$ to $2\pi$.
+        stage (None or ndarray): Flat ``np.ndarray`` that contains
+        ``self.shape`` values of sine from $0$ to $2\pi$.
 
-	static (bool, optional): Boolean variable that determines whether peak
-	is stationary at each time step or moves autonomously at each time step.
+        static (bool, optional): Boolean variable that determines whether peak
+        is stationary at each time step or moves autonomously at each time step.
     """
     def __init__(self,
                  shape,
@@ -105,8 +105,8 @@ class SinusoidalCircle(Environment):
 
     def step(self):
         """
-	If ``self.static`` is ``False``, move peak counterclockwise by
-	``self.dt``.
+        If ``self.static`` is ``False``, move peak counterclockwise by
+        ``self.dt``.
         """
         if not self.static:
             self.phase = (self.phase + self.dt) % (2 * np.pi)
@@ -118,15 +118,15 @@ class SinusoidalCircle(Environment):
     
     def left_eye(self, heading):
         """
-	Returns the information observed by the left eye.
+        Returns the information observed by the left eye.
 
-	Args:
-	    heading (float): Gives the heading in radians from which to gather
-	    left eye information.
+        Args:
+            heading (float): Gives the heading in radians from which to gather
+            left eye information.
 
         Returns:
-	    An ``np.ndarray`` containing values from ``self.stage`` that are $\frac{\pi}{2}$ radians
-	    counterclockwise to ``heading``.
+            An ``np.ndarray`` containing values from ``self.stage`` that are $\frac{\pi}{2}$ radians
+            counterclockwise to ``heading``.
         """
         heading = heading % (2 * np.pi)
         step_size = (2 * np.pi) / self.shape
@@ -135,17 +135,17 @@ class SinusoidalCircle(Environment):
                                mode='wrap')
 
     def right_eye(self, heading):
-	"""
-	Returns the information observed by the right eye.
+        """
+        Returns the information observed by the right eye.
 
-	Args:
-	    heading (float): Gives the heading in radians from which to gather
-	    right eye information.
+        Args:
+            heading (float): Gives the heading in radians from which to gather
+            right eye information.
 
-	Returns:
-	    An ``np.ndarray`` containing values from ``self.stage`` that are
-	    $\frac{\pi}{2}$ radians clockwise to ``heading``.
-	"""
+        Returns:
+            An ``np.ndarray`` containing values from ``self.stage`` that are
+            $\frac{\pi}{2}$ radians clockwise to ``heading``.
+        """
         heading = heading % (2 * np.pi)
         step_size = (2 * np.pi) / self.shape
         index = int(np.clip(heading / step_size, 0, self.shape))
