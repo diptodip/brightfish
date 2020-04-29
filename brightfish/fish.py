29eacabea5affe541b502ac432bebad2b252fb8c
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-04-28 21:45:46 -0400

    Add back updated implementation of MonocularFish

--- brightfish/fish.py
+++ brightfish/fish.py
@@ -499,3 +499,230 @@ class BinocularFish(Fish):
                     params[key] = [step_params[key]]
         return params
 
+class MonocularFish(Fish):
+    """
+    Model zebrafish that separately uses monocular information to update one
+    set point of brightness for each eye.
+
+    Args:
+        heading (float): Defines the heading in radians of the fish. The fish
+        exists in environments defined by 2D arrays where a heading of $0$
+        points directly to the right of the array from whatever position in the
+        array the fish is in.
+
+        position (list of ints): Defines the position of the fish as an index
+        into a 2D array.
+
+        set_point (tuple, optional): A tuple of floats that defines the set
+        points of each eye (left, right) of the fish, i.e. the intensity/ies
+        that the fish should seek to turn towards.
+
+        max_diff (float, optional): Defines the maximum difference between the
+        brightness observed by an eye and the set point that should be
+        considered. This is used to normalize the brightness difference between
+        an observation from an eye and the set point when deciding how to
+        interpolate between the no error and high error turn distribution
+        samples.
+
+        learning_rate (float, optional): Defines how fast the fish updates its
+        set point and turning probabilities.
+
+        p_move (float, optional): Defines probability of moving on a given time
+        step.
+
+        move_dist (tuple, optional): A tuple  of floats defining the mean and
+        standard deviation of move distance on a given time step.
+
+        no_turn_dist (tuple, optional): A tuple of floats defining the mean and
+        standard deviation of heading change in radians when there is no
+        difference between the observed brightnesses in each eye and the set
+        point.
+        
+        left_turn_dist (tuple, optional): A tuple of floats defining the mean
+        and standard deviation of heading change in radians when there is a
+        difference of ``self.max_diff`` between the right eye and the set
+        point, meaning the fish is turning left. In our coordinate system, a
+        change of positive radians is a left (counterclockwise) turn.
+        
+        right_turn_dist (tuple, optional): A tuple of floats defining the mean
+        and standard deviation of heading change in radians when there is a
+        difference of ``self.max_diff`` between the left eye and the set point,
+        meaning the fish is turning right. In our coordinate system, a change
+        of negative radians is a right (clockwise) turn.
+	
+    Attributes:
+        heading (float): Defines the heading in radians of the fish. The fish
+        exists in environments defined by 2D arrays where a heading of $0$
+        points directly to the right of the array from whatever position in the
+        array the fish is in.
+
+        position (list of ints): Defines the position of the fish as an index
+        into a 2D array.
+
+        set_point (tuple): A tuple of floats that defines the set points of
+        each eye (left, right) of the fish, i.e. the intensity/ies that the
+        fish should seek to turn towards.
+
+        max_diff (float): Defines the maximum difference between the brightness
+        observed by an eye and the set point that should be considered. This is
+        used to normalize the brightness difference between an observation from
+        an eye and the set point when deciding how to interpolate between the
+        no error and high error turn distribution samples.
+
+        learning_rate (float): Defines how fast the fish updates its set point
+        and turning probabilities.
+
+        p_move (float): Defines probability of moving on a given time step.
+
+        move_dist (tuple): A tuple  of floats defining the mean and standard
+        deviation of move distance on a given time step.
+
+        no_turn_dist (tuple): A tuple of floats defining the mean and standard
+        deviation of heading change in radians when there is no difference
+        between the observed brightnesses in each eye and the set point.
+        
+        left_turn_dist (tuple): A tuple of floats defining the mean and
+        standard deviation of heading change in radians when there is a
+        difference of ``self.max_diff`` between the right eye and the set
+        point, meaning the fish is turning left. In our coordinate system, a
+        change of positive radians is a left (counterclockwise) turn.
+        
+        right_turn_dist (tuple): A tuple of floats defining the mean and
+        standard deviation of heading change in radians when there is a
+        difference of ``self.max_diff`` between the left eye and the set point,
+        meaning the fish is turning right. In our coordinate system, a change
+        of negative radians is a right (clockwise) turn.
+    """
+    def __init__(self,
+                 heading,
+                 position,
+                 static=False,
+                 set_point=(0.5, 0.5),
+                 max_diff=0.75,
+                 learning_rate=5e-2,
+                 p_move=0.2,
+                 move_dist={'mu': 1.0, 'sigma': 1.0},
+                 no_turn_dist={'mu': 0.01, 'sigma': 0.50},
+                 left_turn_dist={'mu': 0.52, 'sigma': 0.59},
+                 right_turn_dist={'mu': -0.52, 'sigma': 0.59}):
+        super(MonocularFish, self).__init__(heading,
+                                            position,
+                                            static=static,
+                                            set_point=set_point,
+                                            max_diff=max_diff,
+                                            learning_rate=learning_rate,
+                                            p_move=p_move,
+                                            move_dist=move_dist,
+                                            no_turn_dist=no_turn_dist,
+                                            left_turn_dist=left_turn_dist,
+                                            right_turn_dist=right_turn_dist)
+    
+    def turn(self, environment):
+        """
+        Updates ``self.heading`` by an angle chosen as a weighted mixture of
+        samples from the current Fish's high error turning distribution and no
+        error turning distribution.
+        """
+        # calculate left and right eye differences
+        brightness_left = self.brightness_left(environment)
+        brightness_right = self.brightness_right(environment)
+        # left and right eye use different set points
+        diff_left = abs(brightness_left - self.set_point[0]) / self.max_diff
+        diff_right = abs(brightness_right - self.set_point[1]) / self.max_diff
+        # calculate turn angle in radians
+        if diff_left > diff_right:
+            # turning more to the right (clockwise)
+            no_turn_rad = np.random.normal(self.no_turn_dist['mu'],
+                                           self.no_turn_dist['sigma'])
+            turn_rad = np.random.normal(self.right_turn_dist['mu'],
+                                        self.right_turn_dist['sigma'])
+            theta = ((1 - diff_left) * no_turn_rad) + (diff_left * turn_rad)
+        else:
+            # turning more to the left (counterclockwise)
+            no_turn_rad = np.random.normal(self.no_turn_dist['mu'],
+                                           self.no_turn_dist['sigma'])
+            turn_rad = np.random.normal(self.left_turn_dist['mu'],
+                                        self.left_turn_dist['sigma'])
+            theta = ((1 - diff_right) * no_turn_rad) + (diff_left * turn_rad)
+        # update heading by theta radians
+        if not self.static:
+            self.heading += theta
+            self.heading = self.heading % (2 * np.pi)
+        # return calculated update
+        return theta
+
+    def step(self, environment):
+        """
+        Defines the behavior of the fish in one time step in the given
+        environment. This fish takes separate brightness information from each
+        eye and updates a set point for each eye to be closer to the
+        corresponding brightness information.
+
+	Args:
+	    environment (``Environment``): Defines the environment in which the
+	    fish takes a step.
+
+	Returns:
+	    A list of the parameters defining the status of the fish.
+        """
+        # collect brightness information from both eyes
+        brightness_left = self.brightness_left(environment)
+        brightness_right = self.brightness_right(environment)
+
+        # calculate differences
+        diff_left = abs(brightness_left - self.set_point)
+        diff_right = abs(brightness_right - self.set_point)
+        
+        # update set point to be closer to mean of two eyes
+        update_left = self.set_point[0] - brightness_left
+        update_right = self.set_point[1] - brightness_right
+        self.set_point = (self.set_point[0] - learning_rate * update_left,
+                          self.set_point[1] - learning_rate * update_right)
+
+        # move fish
+        (move_distance, theta) = self.move(environment)
+
+        # step environment
+        environment.step()
+
+        # return updated parameters
+        return {'heading': self.heading,
+                'r': self.position[0],
+                'c': self.position[1],
+                'set_point': self.set_point,
+                'diff_left': diff_left,
+                'diff_right': diff_right,
+                'move_distance': move_distance,
+                'theta': theta}
+
+    def run(self, environment, timesteps):
+        """
+	Defines the behavior of the fish over multiple time steps.
+
+	Args:
+	    environment (``Environment``): Defines the environment in which the
+	    fish takes a step.
+
+	    timesteps (int): Defines the number of time steps to perform.
+
+	Returns:
+	    An ``np.ndarray`` of the parameters defining the status of the fish
+	    at each time point.
+        """
+        params = {'heading': [self.heading],
+                  'r': [self.position[0]],
+                  'c': [self.position[1]],
+                  'set_point': [self.set_point],
+                  'diff_left': [0],
+                  'diff_right': [0],
+                  'move_distance': [0],
+                  'theta': [0]}
+        for i in range(timesteps):
+            step_params = self.step(environment)
+            for key in step_params.keys():
+                if key in params:
+                    params[key].append(step_params[key])
+                else:
+                    params[key] = [step_params[key]]
+        return params
+
