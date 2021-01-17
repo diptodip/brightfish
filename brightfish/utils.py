e92185d5656dec5db3c5e1648d0a4a9c2bd5f7dc
Diptodip Deb <dd@d2d.sh>
2021-01-17 15:39:50 -0500

    Fix formatting

--- brightfish/utils.py
+++ brightfish/utils.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+
 def pol2cart(rho, phi, origin=[0, 0]):
     r = -rho * np.sin(phi)
     c = rho * np.cos(phi)
     r += origin[0]
     c += origin[1]
     return (r, c)
+
 
 def nonlinearity(x):
     return np.clip(abs(x) ** (1 / 3), 0.0, 1.0)
